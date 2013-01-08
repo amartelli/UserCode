63742b2aa3156cebcb7a062fe68a77802d836b6b
amartell <>
2013-01-08 14:27:19 +0000

    first commit

---
+++ MassScaleStudies/NonGlobe/lanciaMZAll.sh
@@ -0,0 +1,4 @@
+./trisCheckScale_MZAll EB HIGH scE_reg 1 14000 1112 false   
+./trisCheckScale_MZAll EB LOW scE_reg  1 38000 1112 false    
+./trisCheckScale_MZAll notEBEB HIGH scE_reg 1 7500 1112 false
+./trisCheckScale_MZAll notEBEB LOW scE_reg 1 24000 1112 false 

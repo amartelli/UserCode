8d12747069d4e342d674a8833c21c020bd0bdbe7
amartell <>
2013-01-08 13:53:11 +0000

    first commit

---
+++ MassScaleStudies/GlobeReweighted/lancia.sh
@@ -0,0 +1,11 @@
+./trisCheckScale_Allcategories EB HIGH scE_reg 1 3000000 2012  false
+./trisCheckScale_Allcategories EE HIGH scE_reg 1 3000000 2012  false
+
+./trisCheckScale_Allcategories EB HIGH scE_reg 1 2000000 2011 false
+./trisCheckScale_Allcategories EE HIGH scE_reg 1 2000000 2011 false
+
+./trisCheckScale_Allcategories EB LOW scE_reg 1 3000000 2012 false
+./trisCheckScale_Allcategories EE LOW scE_reg 1 3000000 2012 false
+
+./trisCheckScale_Allcategories EB LOW scE_reg 1 2000000 2011 false
+./trisCheckScale_Allcategories EE LOW scE_reg 1 2000000 2011 false

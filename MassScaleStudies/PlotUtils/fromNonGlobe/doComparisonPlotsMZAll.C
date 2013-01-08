3c57ad577138c004a0f2887edbe8240b2d91a4a0
amartell <>
2013-01-08 16:03:37 +0000

    check ok

--- MassScaleStudies/PlotUtils/fromNonGlobe/doComparisonPlotsMZAll.C
+++ MassScaleStudies/PlotUtils/fromNonGlobe/doComparisonPlotsMZAll.C
@@ -4,48 +4,51 @@
 #include <iostream>
 #include <algorithm>
 
-void doComparisonPlotsMZAll(){
+void doComparisonPlots(){
 
   //  gROOT->ProcessLine(".x /Users/Arabella/Public/style.C");
 
-  std::string plotDir = "../../NonGlobe/PLOTS_MZAll";
-  std::string plotDirOut = "2011vs2012_MZAll";
-//   float xmin = -0.005;
-//   float xmax = 0.015;
+  std::string plotDir = "PLOTS_MZ";
+  std::string plotDirOut = "2011vs2012_MZ";
 
-//      std::string plotDir = "PLOTS_MZAll_Sh";
-//      std::string plotDirOut = "2011vs2012_MZAll_Sh";
-  float xmin = -0.005;
-  float xmax = 0.015;
+//    std::string plotDir = "PLOTS_MZ_Sh";
+//    std::string plotDirOut = "2011vs2012_MZ_Sh";
 
-//     std::string plotDir = "PLOTS_MZP";
-//     std::string plotDirOut = "2011vs2012_MZP";
-
-//         std::string plotDir = "PLOTS_MZP_Sh";
-//         std::string plotDirOut = "2011vs2012_MZP_Sh";
+//       std::string plotDir = "PLOTS_MZmom";
+//       std::string plotDirOut = "2011vs2012_MZmom";
      
   for(int ii=0; ii<4; ++ii){
-    std::string category; 
+    std::string category;
     if(ii == 0)  category = "EB_HIGH_scE_reg";
     if(ii == 1)  category = "EB_LOW_scE_reg";
     if(ii == 2)  category = "notEBEB_HIGH_scE_reg";
     if(ii == 3)  category = "notEBEB_LOW_scE_reg";
 
-    std::string file1112 = plotDir+"/results_"+category+"_1112.root";
+    std::string file2011 = plotDir+"/results_"+category+"_2011.root";
+    std::string file2012 = plotDir+"/results_"+category+"_2012.root";
 
-    TFile f1112(file1112.c_str(),"read");
+    TFile f2012(file2012.c_str(),"read");
+    TFile f2011(file2011.c_str(),"read");
 
-   TGraphErrors* graph1112 = (TGraphErrors*)f1112.Get("finalGraph");
-
-   if(ii == 0 || ii == 2)   graph1112->SetMarkerColor(kGreen+2);
-   if(ii == 1 || ii == 3)   graph1112->SetMarkerColor(kRed+2);
-
+   TGraphErrors* graph2012 = (TGraphErrors*)f2012.Get("finalGraph");
+   TGraphErrors* graph2011 = (TGraphErrors*)f2011.Get("finalGraph");
 
    if(ii == 0 || ii == 2){
-     graph1112->SetMarkerStyle(20);
+     graph2012->SetMarkerColor(kGreen+2);
+     graph2011->SetMarkerColor(kRed+2);
    }
    if(ii == 1 || ii == 3){
-     graph1112->SetMarkerStyle(21);
+     graph2012->SetMarkerColor(kOrange-3);
+     graph2011->SetMarkerColor(kAzure+8);
+   }
+
+   if(ii == 0 || ii == 2){
+     graph2012->SetMarkerStyle(20);
+     graph2011->SetMarkerStyle(20);
+   }
+   if(ii == 1 || ii == 3){
+     graph2012->SetMarkerStyle(21);
+     graph2011->SetMarkerStyle(21);
    }
 
   TCanvas* cplot = new TCanvas("gplot", "gplot",100,100,725,500);
@@ -64,7 +67,7 @@ void doComparisonPlotsMZAll(){
   cLeft->SetGridy();
 
   // pad settings                                                                     
-  TH1F *hPad = (TH1F*)gPad->DrawFrame(20.,xmin,150.,xmax);
+  TH1F *hPad = (TH1F*)gPad->DrawFrame(20.,-0.005,150.,0.01);
   hPad->GetXaxis()->SetTitle("H_{T}");
   hPad->GetYaxis()->SetTitle("(Mee/MZ)_{data}-(Mee/MZ)_{mc}");
   hPad->GetYaxis()->SetTitleOffset(tYoffset);
@@ -77,20 +80,24 @@ void doComparisonPlotsMZAll(){
   tspec->SetFillColor(0);
   tspec->SetTextFont(42);
   if(ii == 0){
-  tspec->AddEntry(graph1112,"2011+2012 EB hR9","PL");
+  tspec->AddEntry(graph2012,"2012 EB hR9","PL");
+  tspec->AddEntry(graph2011,"2011 EB hR9","PL");
   }
   if(ii == 1){
-  tspec->AddEntry(graph1112,"2011+2012 EB lR9","PL");
+  tspec->AddEntry(graph2012,"2012 EB lR9","PL");
+  tspec->AddEntry(graph2011,"2011 EB lR9","PL");
   }
   if(ii == 2){
-  tspec->AddEntry(graph1112,"2011+2012 !EBEB hR9","PL");
+  tspec->AddEntry(graph2012,"2012 !EBEB hR9","PL");
+  tspec->AddEntry(graph2011,"2011 !EBEB hR9","PL");
   }
   if(ii == 3){
-  tspec->AddEntry(graph1112,"2011+2012 !EBEB lR9","PL");
+  tspec->AddEntry(graph2012,"2012 !EBEB lR9","PL");
+  tspec->AddEntry(graph2011,"2011 !EBEB lR9","PL");
   }
 
-  graph1112->Draw("P");
-  //  graph2011->Draw("P,same");
+  graph2012->Draw("P");
+  graph2011->Draw("P,same");
 
   tspec->Draw("same");
   cplot->Print( (plotDirOut+"/EoP_vs_Et_"+category+".png").c_str(),".png");
@@ -100,20 +107,33 @@ void doComparisonPlotsMZAll(){
   }
 
   // tutto EB in 1 plot
-  std::string f1112_h = plotDir+"/results_EB_HIGH_scE_reg_1112.root";
-  std::string f1112_l = plotDir+"/results_EB_LOW_scE_reg_1112.root";
+  std::string f11_h = plotDir+"/results_EB_HIGH_scE_reg_2011.root";
+  std::string f12_h = plotDir+"/results_EB_HIGH_scE_reg_2012.root";
+  std::string f11_l = plotDir+"/results_EB_LOW_scE_reg_2011.root";
+  std::string f12_l = plotDir+"/results_EB_LOW_scE_reg_2012.root";
 
-  TFile F1112_h(f1112_h.c_str(),"read");
-  TFile F1112_l(f1112_l.c_str(),"read");
+  TFile F12_h(f12_h.c_str(),"read");
+  TFile F11_h(f11_h.c_str(),"read");
 
-  TGraphErrors* g1112_h = (TGraphErrors*)F1112_h.Get("finalGraph");
-  TGraphErrors* g1112_l = (TGraphErrors*)F1112_l.Get("finalGraph");
+  TFile F12_l(f12_l.c_str(),"read");
+  TFile F11_l(f11_l.c_str(),"read");
 
-    g1112_h->SetMarkerColor(kGreen+2);
-    g1112_l->SetMarkerColor(kRed+2);
+  TGraphErrors* g2012_h = (TGraphErrors*)F12_h.Get("finalGraph");
+  TGraphErrors* g2011_h = (TGraphErrors*)F11_h.Get("finalGraph");
+  TGraphErrors* g2012_l = (TGraphErrors*)F12_l.Get("finalGraph");
+  TGraphErrors* g2011_l = (TGraphErrors*)F11_l.Get("finalGraph");
 
-    g1112_h->SetMarkerStyle(20);
-    g1112_l->SetMarkerStyle(21);
+
+  g2012_h->SetMarkerColor(kGreen+2);
+  g2012_l->SetMarkerColor(kOrange-3);
+  g2011_h->SetMarkerColor(kRed+2);
+  g2011_l->SetMarkerColor(kAzure+8);
+
+
+    g2012_h->SetMarkerStyle(20);
+    g2011_h->SetMarkerStyle(20);
+    g2012_l->SetMarkerStyle(21);
+    g2011_l->SetMarkerStyle(21);
 
   TCanvas* c = new TCanvas("g", "g",100,100,725,500);
   c->cd();
@@ -131,7 +151,7 @@ void doComparisonPlotsMZAll(){
   cLeft->SetGridy();
     
   // pad settings                                                                     
-  TH1F *hPad = (TH1F*)gPad->DrawFrame(20,xmin,150,xmax);
+  TH1F *hPad = (TH1F*)gPad->DrawFrame(20,-0.005,150,0.01);
   hPad->GetXaxis()->SetTitle("H_{T}");
   hPad->GetYaxis()->SetTitle("(Mee/MZ)_{data}-(Mee/MZ)_{mc}");
   hPad->GetYaxis()->SetTitleOffset(tYoffset);
@@ -145,34 +165,50 @@ void doComparisonPlotsMZAll(){
   tspec->SetFillColor(0);
   tspec->SetTextFont(42);
 
-  tspec->AddEntry(g1112_h,"2011+2012 EB hR9","PL");
-  tspec->AddEntry(g1112_l,"2011+2012 EB lR9","PL");
+  tspec->AddEntry(g2012_h,"2012 EB hR9","PL");
+  tspec->AddEntry(g2011_h,"2011 EB hR9","PL");
+  tspec->AddEntry(g2012_l,"2012 EB lR9","PL");
+  tspec->AddEntry(g2011_l,"2011 EB lR9","PL");
 
-  g1112_h->Draw("p");
-  g1112_l->Draw("p, same");
+  g2012_h->Draw("p");
+  g2011_h->Draw("p, same");
+  g2012_l->Draw("p, same");
+  g2011_l->Draw("p, same");
     
   tspec->Draw("same");
   c->Print( (plotDirOut+"/EoP_vs_Et_EB.png").c_str(),".png");
 
   ///////////////////////////////////////////////////////
   // tutto EE in 1 plot
-  std::string f1112E_h = plotDir+"/results_notEBEB_HIGH_scE_reg_1112.root";
-  std::string f1112E_l = plotDir+"/results_notEBEB_LOW_scE_reg_1112.root";
+  std::string f11_hE = plotDir+"/results_notEBEB_HIGH_scE_reg_2011.root";
+  std::string f12_hE = plotDir+"/results_notEBEB_HIGH_scE_reg_2012.root";
+  std::string f11_lE = plotDir+"/results_notEBEB_LOW_scE_reg_2011.root";
+  std::string f12_lE = plotDir+"/results_notEBEB_LOW_scE_reg_2012.root";
 
-  TFile F1112E_h(f1112E_h.c_str(),"read");
-  TFile F1112E_l(f1112E_l.c_str(),"read");
+  TFile F12_hE(f12_hE.c_str(),"read");
+  TFile F11_hE(f11_hE.c_str(),"read");
 
-  TGraphErrors* g1112E_h = (TGraphErrors*)F1112E_h.Get("finalGraph");
-  TGraphErrors* g1112E_l = (TGraphErrors*)F1112E_l.Get("finalGraph");
+  TFile F12_lE(f12_lE.c_str(),"read");
+  TFile F11_lE(f11_lE.c_str(),"read");
 
-    g1112E_h->SetMarkerColor(kGreen+2);
-    g1112E_l->SetMarkerColor(kRed+2);
+  TGraphErrors* g2012_hE = (TGraphErrors*)F12_hE.Get("finalGraph");
+  TGraphErrors* g2011_hE = (TGraphErrors*)F11_hE.Get("finalGraph");
+  TGraphErrors* g2012_lE = (TGraphErrors*)F12_lE.Get("finalGraph");
+  TGraphErrors* g2011_lE = (TGraphErrors*)F11_lE.Get("finalGraph");
 
-    g1112E_h->SetMarkerStyle(20);
-    g1112E_l->SetMarkerStyle(21);
 
-  TCanvas* c = new TCanvas("g", "g",100,100,725,500);
-  c->cd();
+  g2012_hE->SetMarkerColor(kGreen+2);
+  g2012_lE->SetMarkerColor(kOrange-3);
+  g2011_hE->SetMarkerColor(kRed+2);
+  g2011_lE->SetMarkerColor(kAzure+8);
+
+    g2012_hE->SetMarkerStyle(20);
+    g2011_hE->SetMarkerStyle(20);
+    g2012_lE->SetMarkerStyle(21);
+    g2011_lE->SetMarkerStyle(21);
+
+  TCanvas* cE = new TCanvas("gE", "gE",100,100,725,500);
+  cE->cd();
   TPad *cLeft  = new TPad("pad_0","pad_0",0.00,0.00,1.00,1.00);
   cLeft->SetLeftMargin(0.17);
   cLeft->SetRightMargin(0.025);
@@ -187,7 +223,7 @@ void doComparisonPlotsMZAll(){
   cLeft->SetGridy();
     
   // pad settings                                                                     
-  TH1F *hPad = (TH1F*)gPad->DrawFrame(20,xmin,150,xmax);
+  TH1F *hPad = (TH1F*)gPad->DrawFrame(20,-0.005,150,0.01);
   hPad->GetXaxis()->SetTitle("H_{T}");
   hPad->GetYaxis()->SetTitle("(Mee/MZ)_{data}-(Mee/MZ)_{mc}");
   hPad->GetYaxis()->SetTitleOffset(tYoffset);
@@ -197,19 +233,22 @@ void doComparisonPlotsMZAll(){
   hPad->GetYaxis()->SetLabelSize(labSize);
   hPad->GetYaxis()->SetTitleSize(labSize);
 
-  TLegend *tspec = new TLegend(0.64,0.80,0.99,0.99);
-  tspec->SetFillColor(0);
-  tspec->SetTextFont(42);
+  TLegend *tspecE = new TLegend(0.64,0.80,0.99,0.99);
+  tspecE->SetFillColor(0);
+  tspecE->SetTextFont(42);
 
-  tspec->AddEntry(g1112E_h,"2011+2012 !EBEB hR9","PL");
-  tspec->AddEntry(g1112E_l,"2011+2012 !EBEB lR9","PL");
+  tspecE->AddEntry(g2012_hE,"2012 !EBEB hR9","PL");
+  tspecE->AddEntry(g2011_hE,"2011 !EBEB hR9","PL");
+  tspecE->AddEntry(g2012_lE,"2012 !EBEB lR9","PL");
+  tspecE->AddEntry(g2011_lE,"2011 !EBEBE lR9","PL");
 
-  g1112E_h->Draw("p");
-  g1112E_l->Draw("p, same");
+  g2012_hE->Draw("p");
+  g2011_hE->Draw("p, same");
+  g2012_lE->Draw("p, same");
+  g2011_lE->Draw("p, same");
     
-  tspec->Draw("same");
-  c->Print( (plotDirOut+"/EoP_vs_Et_notEBEB.png").c_str(),".png");
-
+  tspecE->Draw("same");
+  cE->Print( (plotDirOut+"/EoP_vs_Et_notEBEB.png").c_str(),".png");
 
 }
 

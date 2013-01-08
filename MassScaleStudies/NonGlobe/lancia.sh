8fc167a2f65ad3abd7b585938f9840059d5c6f99
amartell <>
2013-01-08 12:59:06 +0000

    first commit

---
+++ MassScaleStudies/NonGlobe/lancia.sh
@@ -0,0 +1,12 @@
+#./trisCheckScale_Allcategories EB HIGH scE_reg 1 50000 2012  false
+#./trisCheckScale_Allcategories EE HIGH scE_reg 1 50000 2012  false
+
+#./trisCheckScale_Allcategories EB LOW scE_reg 1 90000 2012 false
+#./trisCheckScale_Allcategories EE LOW scE_reg 1 90000 2012 false
+
+
+./trisCheckScale_Allcategories EB HIGH scE_reg 1 30000 2011 false 
+./trisCheckScale_Allcategories EE HIGH scE_reg 1 30000 2011 false
+
+./trisCheckScale_Allcategories EB LOW scE_reg 1 50000 2011 false
+./trisCheckScale_Allcategories EE LOW scE_reg 1 50000 2011 false

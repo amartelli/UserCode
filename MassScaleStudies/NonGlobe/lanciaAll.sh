18643314ef484594ce9c423b3348241dc8690bfe
amartell <>
2013-01-09 21:35:07 +0000

    check ok

---
+++ MassScaleStudies/NonGlobe/lanciaAll.sh
@@ -0,0 +1,18 @@
+#./trisCheckScale_All EB HIGH scE_reg 1 50000 2012  false Et
+#./trisCheckScale_All EE HIGH scE_reg 1 50000 2012  false Et
+
+#./trisCheckScale_All EB LOW scE_reg 1 90000 2012 false Et
+#./trisCheckScale_All EE LOW scE_reg 1 90000 2012 false Et
+
+#./trisCheckScale_All EB HIGH scE_reg 1 30000 2011 false Et
+#./trisCheckScale_All EE HIGH scE_reg 1 30000 2011 false Et
+
+#./trisCheckScale_All EB LOW scE_reg 1 50000 2011 false Et
+#./trisCheckScale_All EE LOW scE_reg 1 50000 2011 false Et
+
+
+./trisCheckScale_All EB none scE_reg 1 80000 2012 false R9
+./trisCheckScale_All EE none scE_reg 1 24000 2012 false R9
+
+./trisCheckScale_All EB none scE_reg 1 60000 2011 false R9
+./trisCheckScale_All EE none scE_reg 1 20000 2011 false R9

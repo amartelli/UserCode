40750a1f15c135717f9e90c72eac9baf9f97d139
abenagli <>
2013-01-10 16:22:47 +0000

    updates

--- MassScaleStudies/Macros/ComputePUweights.C
+++ MassScaleStudies/Macros/ComputePUweights.C
@@ -1,49 +1,61 @@
 //****** simple macro to compute PU weights ******
+
+
+void ComputePUweights()
 {
-  /*
-  //*** mc file - 2012
-  TChain* ntu = new TChain("simpleNtupleEoverP/SimpleNtupleEoverP");
-  ntu->Add("/media/DATA/ALCARECO/DYSummer12/DYJets-Summer12.root");
-  TH1F* hmc = new TH1F("hmc","hmc",100,0.,100.);
-  ntu->Draw("PUit_TrueNumInteractions >> hmc","","goff");
-  */
-
-  //*** mc file - 2011
-
-  //   TChain* ntu = new TChain("simpleNtupleEoverP/SimpleNtupleEoverP");
-   //   ntu->Add("/tmp/amartell/DYJToLL_M50_TuneZ2S_8TeV-mad_Summer12_DR53X-PU_S10_START53_V7A-v1.root");
-   TChain* ntu = new TChain("simpleNtupleEoverPSh/SimpleNtupleEoverP");
-   ntu->Add("/tmp/amartell/DYJets-Summer12-START53-noSkim.root");
-   TH1F* hmc = new TH1F("hmc","hmc",100,0.,100.);
-   //  ntu->Draw("PUit_NumInteractions >> hmc","","goff");
-   ntu->Draw("PUit_TrueNumInteractions >> hmc","","goff");
-
-
-  /*
-  //*** data file 2012
-  //TFile* fda = TFile::Open("True_PilePU_PostRerecoICHEP2012.root");
-  //  TFile* fda = TFile::Open("True_PilePU_Prompt2012.root");
-  TFile* fda = TFile::Open("Observed_PilePU_Prompt2012.root");
-  */
-
-  //*** data file 2011
-  TFile* fda = TFile::Open("PUweights_DYJetsToLL_Summer12_ABC_TrueNumInteractions.root");
-
-  TH1F* pileup = (TH1F*)fda->Get("pileup");
-
-  TH1F* hdata = new TH1F("hdata","hdata",100,0,100);
-  //TH1F* hdata = new TH1F("hdata","hdata",100,0,100);
-  for(int ibin = 1; ibin < 101; ibin++){
-    hdata->SetBinContent(ibin, pileup->GetBinContent(ibin));
+  // histograms
+  TH1F* h_mc = new TH1F("h_mc","",100,-0.5,99.5);
+  TH1F* h_da = new TH1F("h_da","",100,-0.5,99.5);
+  TH1F* temp = NULL;
+  
+  // files
+  TFile* f_mc = TFile::Open("../Pileup/pileup_DYToEE_M-20_CT10_TuneZ2star_v2_8TeV-powheg-pythia6_Summer12_DR53X-PU_S10_START53_V7A-v1_AODSIM.root");
+  TFile* f_da = TFile::Open("../Pileup/pileup_69p3mb_true_Moriond2013.root");
+  
+  
+  
+  // fill mc histogram
+  std::cout << "\n\n\n>>> MC pileup histogram" << std::endl;
+  
+  temp = (TH1F*)( f_mc->Get("PUDumper/nPUtrue") );
+  for(int bin = 1; bin <= temp->GetNbinsX(); ++bin)
+  {
+    float binCenter  = temp -> GetBinCenter(bin);
+    float binContent = temp -> GetBinContent(bin);
+    std::cout << "bin: " << bin << "   binCenter: " << binCenter << "   binContent: " << binContent << std::endl;
+    
+    h_mc -> Fill(binCenter,binContent);
   }
- 
-  //*** compute weights
-  TH1F* hweights = (TH1F*)hdata->Clone("hweights");
-  hweights->Reset();
-  hweights->Divide(hdata, hmc, 1./hdata->GetSumOfWeights(), 1./hmc->GetSumOfWeights());
-  //hweights->Divide(hdata, hmc, 1., 1.);
-  TFile* fout = new TFile("PUweights_DYJetsToLL_Summer12_53X_ShSkim_ABC_TrueNumInteractions.root","recreate");
-  hweights->Write("hweights");
-  fout->Close();
-
+  h_mc -> Scale(1./h_mc->Integral());
+  std::cout << "Integral: " << h_mc -> Integral() << std::endl;
+  
+  
+  // fill da histogram
+  std::cout << "\n\n\n>>> DA pileup histogram" << std::endl;
+  
+  temp = (TH1F*)( f_da->Get("pileup") );
+  for(int bin = 1; bin <= temp->GetNbinsX(); ++bin)
+  {
+    float binCenter  = temp -> GetBinCenter(bin) - 0.5;
+    float binContent = temp -> GetBinContent(bin);
+    std::cout << "bin: " << bin << "   binCenter: " << binCenter << "   binContent: " << binContent << std::endl;
+    
+    h_da -> Fill(binCenter,binContent);
+  }
+  h_da -> Scale(1./h_da->Integral());
+  std::cout << "Integral: " << h_da -> Integral() << std::endl;
+  
+  
+  // save PU weights
+  TFile* f = new TFile("../Pileup/pileup_69p3mb_true_Moriond2013__Summer12_DR53X-PU_S10_START53.root","RECREATE");
+  f -> cd();
+  
+  TH1F* h_PUweights = (TH1F*)(h_da->Clone("h_PUweights"));
+  h_PUweights -> Divide(h_mc);
+  
+  h_da -> Write();
+  h_mc -> Write();
+  h_PUweights -> Write();
+  
+  f -> Close();
 }

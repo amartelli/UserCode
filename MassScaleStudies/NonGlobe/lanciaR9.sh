6868020cf132726969a58bd947f397ec5e6510a4
amartell <>
2013-01-10 16:16:31 +0000

    now merged

--- MassScaleStudies/NonGlobe/lanciaR9.sh
+++
@@ -1,5 +0,0 @@
-./trisCheckScale_AllR9 EB scE_reg 1 80000 2012
-./trisCheckScale_AllR9 EE scE_reg 1 24000 2012
-
-./trisCheckScale_AllR9 EB scE_reg 1 60000 2011
-./trisCheckScale_AllR9 EE scE_reg 1 20000 2011

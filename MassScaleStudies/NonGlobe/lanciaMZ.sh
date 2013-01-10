f451b558081ee5b52ef006e93353a20d5136e061
amartell <>
2013-01-10 16:32:55 +0000

    first commit

---
+++ MassScaleStudies/NonGlobe/lanciaMZ.sh
@@ -0,0 +1,10 @@
+./trisCheckScale_MZ EBEB HH scE_reg 1 10000 2011 false
+./trisCheckScale_MZ EBEB notHH scE_reg 1 25000 2011 false
+./trisCheckScale_MZ notEBEB HH scE_reg 1 8000 2011 false
+./trisCheckScale_MZ notEBEB notHH scE_reg 1 18000 2011 false
+
+./trisCheckScale_MZ EBEB HH scE_reg 1 7000 2012 false
+./trisCheckScale_MZ EBEB notHH scE_reg 1 25000 2012 false
+./trisCheckScale_MZ notEBEB HH scE_reg 1 8000 2012 false
+./trisCheckScale_MZ notEBEB notHH scE_reg 1 18000 2012 false
+
